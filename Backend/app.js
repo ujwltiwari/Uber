e611dd9d73a9d30fa6a737b97d90e8181bfa1e1f
Ujjwal Kumar <ujwltiwari494@gmail.com>
2025-03-22 22:31:44 +0530

    user auth integration done

--- Backend/app.js
+++ Backend/app.js
@@ -1,38 +1,38 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const express = require("express");
-const cors = require("cors");
-const connectToDB = require("./db/db");
-const app = express();
-const errorHandler = require("./utils/errorHandler");
-const cookieParser = require("cookie-parser");
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const cors = require('cors')
+const connectToDB = require('./db/db')
+const app = express()
+const errorHandler = require('./utils/errorHandler')
+const cookieParser = require('cookie-parser')
 
 // Route Imports
-const userRoutes = require("./routes/user.routes");
-const captainRoutes = require("./routes/captain.routes");
+const userRoutes = require('./routes/user.routes')
+const captainRoutes = require('./routes/captain.routes')
 
-connectToDB();
+connectToDB()
 // Cors
-app.use(cookieParser());
+app.use(cookieParser())
 // app.use(cors());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000", // Your frontend
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000', // Your frontend
     credentials: true, // ✅ Allow cookies to be sent
-  }),
-);
-app.use(express.json()); // Parses JSON request body
-app.use(express.urlencoded({ extended: true })); // Parses URL-encoded data
+  })
+)
+app.use(express.json()) // Parses JSON request body
+app.use(express.urlencoded({ extended: true })) // Parses URL-encoded data
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.use("/users", userRoutes);
-app.use("/captains", captainRoutes);
+app.use('/users', userRoutes)
+app.use('/captains', captainRoutes)
 
 /* Always should be below every route declaration
    Handle the error code & pass the error efficiently */
-app.use(errorHandler);
+app.use(errorHandler)
 
-module.exports = app;
+module.exports = app
